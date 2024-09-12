4dd94df87e5dfbec67ffd0654eb88ae121e47d05
Максим Русецкий <maksim.rusetckiy05@mail.ru>
2024-09-12 19:13:33 +0300

    Connect database in API

--- Library/Library.API/Program.cs
+++ Library/Library.API/Program.cs
@@ -1,10 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Library.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 var configuration = builder.Configuration;
 
+configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
 services.AddControllers();
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
+
+services.AddDbContext<LibraryDbContext>(options =>
+{
+    options.UseNpgsql(configuration.GetConnectionString("LibraryConnection"));
+});
+
 
 var app = builder.Build();
 
