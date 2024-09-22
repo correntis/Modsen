e6b2cfbe5d95b0834b9da7c66b75f2d9814de864
Максим Русецкий <maksim.rusetckiy05@mail.ru>
2024-09-22 11:55:34 +0300

    Add exception middleware

---
+++ Library/Library.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,43 @@
+﻿
+using Library.API.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace Library.API.Middleware
+{
+    public class ExceptionMiddleware : IMiddleware
+    {
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IWebHostEnvironment _env;
+
+        public ExceptionMiddleware(
+            ILogger<ExceptionMiddleware> logger,
+            IWebHostEnvironment env)
+        {
+            _logger = logger;
+            _env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+			try
+			{
+				await next(context);
+			}
+			catch(Exception ex)
+			{
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment()
+                    ? new ApiException(context.Response.StatusCode, ex.Message, ex.ToString())
+                    : new ApiException(context.Response.StatusCode, "Internal Server Error");
+
+                var json = JsonSerializer.Serialize(response);
+
+                await context.Response.WriteAsync(json);
+			}
+        }
+    }
+}
